7f3504db23373b17b7454f3bdc2f45dddc5bb12b
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-03-18 20:44:47 +0200

    Add query by axios req from server

--- public/services/bug.service.js
+++ public/services/bug.service.js
@@ -3,8 +3,8 @@ import { storageService } from './async-storage.service.js'
 import { utilService } from './util.service.js'
 
 const STORAGE_KEY = 'bugDB'
-
-_createBugs()
+const BASE_URL = `api/bug/`
+// _createBugs()
 
 export const bugService = {
     query,
@@ -15,8 +15,11 @@ export const bugService = {
 
 
 function query() {
-    return storageService.query(STORAGE_KEY)
+    return axios.get(BASE_URL)
+            .then(res => res.data)
+            .then(bugs => {return bugs} )
 }
+
 function getById(bugId) {
     return storageService.get(STORAGE_KEY, bugId)
 }
@@ -33,37 +36,34 @@ function save(bug) {
     }
 }
 
+// function _createBugs() {
+//     let bugs = utilService.loadFromStorage(STORAGE_KEY)
+//     if (!bugs || !bugs.length) {
+//         bugs = [
+//             {
+//                 title: "Infinite Loop Detected",
+//                 severity: 4,
+//                 _id: "1NF1N1T3"
+//             },
+//             {
+//                 title: "Keyboard Not Found",
+//                 severity: 3,
+//                 _id: "K3YB0RD"
+//             },
+//             {
+//                 title: "404 Coffee Not Found",
+//                 severity: 2,
+//                 _id: "C0FF33"
+//             },
+//             {
+//                 title: "Unexpected Response",
+//                 severity: 1,
+//                 _id: "G0053"
+//             }
+//         ]
+//         utilService.saveToStorage(STORAGE_KEY, bugs)
+//     }
 
 
 
-function _createBugs() {
-    let bugs = utilService.loadFromStorage(STORAGE_KEY)
-    if (!bugs || !bugs.length) {
-        bugs = [
-            {
-                title: "Infinite Loop Detected",
-                severity: 4,
-                _id: "1NF1N1T3"
-            },
-            {
-                title: "Keyboard Not Found",
-                severity: 3,
-                _id: "K3YB0RD"
-            },
-            {
-                title: "404 Coffee Not Found",
-                severity: 2,
-                _id: "C0FF33"
-            },
-            {
-                title: "Unexpected Response",
-                severity: 1,
-                _id: "G0053"
-            }
-        ]
-        utilService.saveToStorage(STORAGE_KEY, bugs)
-    }
 
-
-
-}
